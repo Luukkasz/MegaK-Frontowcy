4bf3ac9de8697a4d582d1e325dea4dc98747154a
l.machalewski <lukasz.machalewski@currency-one.com>
2024-02-16 11:18:34 +0100

    create step by step fresh react app with webpack and ts

---
+++ my-react-webpack-app/src/App.tsx
@@ -0,0 +1,7 @@
+import {FC} from "react";
+
+const App: FC = () => {
+    return <h1>Hello world form app.tsx</h1>
+}
+
+export {App}
---
+++ my-react-webpack-app/webpack.common.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+              test: /\.ts|tsx$/i,
+              use: 'ts-loader'
+            },
+            {
+                test: /\.js|jsx$/i,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+        ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, 'public/index.html'),
+          filename: 'index.html'
+      })
+    ],
+    resolve: {
+        extensions: [".tsx", ".ts", ".jsx", ".js"]
+    }
+};
