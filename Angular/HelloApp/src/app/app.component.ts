1288a7e01cdc5bbbe3218bcd9e8ea155ce11bc69
l.machalewski <lukasz.machalewski@currency-one.com>
2024-09-16 21:22:00 +0200

    angular component lesson

--- Angular/HelloApp/src/app/app.component.ts
+++ Angular/HelloApp/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NameDisplayerComponent} from "./name-displayer/name-displayer.component";
+import {ParentComponent} from "./parent/parent.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NameDisplayerComponent, ParentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Angular/HelloApp/src/app/name-displayer/name-displayer.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-name-displayer',
+  standalone: true,
+  imports: [],
+  templateUrl: './name-displayer.component.html',
+  styleUrl: './name-displayer.component.scss'
+})
+export class NameDisplayerComponent {
+  @Input()
+  name: string = '';
+
+  displayName: string = ''
+
+  showMyName() {
+    this.displayName = this.name;
+  }
+}
